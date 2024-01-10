a00666ba0b750e6461a98a2417aa1126315225c8
Kaue Miziara <kauemiziara@gmail.com>
2024-01-10 16:10:26 -0300

    Fix identation in pages.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,19 @@ import Courses from '../components/Courses';
 import Footer from '../components/Footer';
 
 export default function Home() {
-  return (
-    <>
-      <h1>Line Care</h1>
-
-      <hr />
-      <Overview />
-      
-      <hr />
-      <About />
-
-      <Courses />
-
-      <Footer />
-    </>
-  )
+    return (
+        <>
+            <h1>Line Care</h1>
+        
+            <hr />
+            <Overview />
+            
+            <hr />
+            <About />
+        
+            <Courses />
+        
+            <Footer />
+        </>
+    )
 }
