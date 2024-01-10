28425814e13a9f5a78815d45e5858a692446578d
Kaue Miziara <kauemiziara@gmail.com>
2024-01-10 20:25:30 -0300

    Add name and description as props to Card component

--- src/components/Courses/Card/index.tsx
+++ src/components/Courses/Card/index.tsx
@@ -1,8 +1,17 @@
-export default function Card() {
+import React from "react";
+
+interface CardProps {
+    cardName: string;
+    description: string;
+}
+
+const Card: React.FC<CardProps> = ({ cardName, description }) => {
     return (
-        <>
-            <p><b>Nome</b>: AAA</p>
-            <p><b>Descrição</b>: BBB</p>
-        </>
+        <div className="card">
+            <p><b>Nome</b>: {cardName}</p>
+            <p><b>Descrição</b>: {description}</p>
+        </div>
     )
 }
+
+export default Card;
--- src/components/Courses/index.tsx
+++ src/components/Courses/index.tsx
@@ -5,9 +5,9 @@ export default function Courses() {
     return (
         <div id="cursos" className="courses-section-container">
             <div className="courses-container">
-                <div className="card">
-                    <Card />
-                </div>
+                <Card cardName="AAA" description="BBB"/>
+                <Card cardName="BBB" description="CCC"/>
+                <Card cardName="CCC" description="DDD"/>
             </div>
         </div>
     )
